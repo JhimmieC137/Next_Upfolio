b9fc949b5c7464f61a99af4650ea3e8f48bf34fe
JhimmieC137 <toluwalope.david@gmail.com>
2024-10-15 20:10:32 +0100

    Added programs page

--- src/page-components/projects/page.tsx
+++ src/page-components/projects/page.tsx
@@ -34,7 +34,7 @@ export default function ProjectsPage() {
                                     <div className="cursor-pointer peer z-10 absolute top-8 right-8 rounded-full bg-white text-zinc-800 h-24 w-24 p-3 flex justify-center  hover:bg-pink-600 hover:text-zinc-100 ease-in-out duration-200">
                                         <ArrowRightIcon className="font-bold m-auto p-1 h-10 w-auto -rotate-45" />
                                     </div>
-                                    <img className='cursor-pointer relative w-full hover:scale-125 peer-hover:scale-125 hover:rotate-6 peer-hover:rotate-3 ease-in-out duration-[4.5s]' src='/webp/blog-3.webp' alt=''/>
+                                    <img className='cursor-pointer relative w-full hover:scale-105 peer-hover:scale-105 hover:rotate-3 peer-hover:rotate-3 ease duration-[3.5s]' src='/webp/blog-3.webp' alt=''/>
                                 </div>
                                 <div className="flex flex-row items-center w-full mt-3">
                                     <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
@@ -56,14 +56,13 @@ export default function ProjectsPage() {
                                     </p>
                                 </div>
                             </div>
-                        
                             
                             <div className="flex flex-col item-center max-w-[36rem]">
                                 <div className='relative w-full max-w-[36rem] max-h-[43rem] xl:rounded-[2rem] rounded-[1rem] mb-3 overflow-hidden'>
                                     <div className="cursor-pointer peer z-10 absolute top-8 right-8 rounded-full bg-white text-zinc-800 h-24 w-24 p-3 flex justify-center  hover:bg-pink-600 hover:text-zinc-100 ease-in-out duration-200">
                                         <ArrowRightIcon className="font-bold m-auto p-1 h-10 w-auto -rotate-45" />
                                     </div>
-                                    <img className='cursor-pointer relative w-full hover:scale-125 peer-hover:scale-125 hover:rotate-6 peer-hover:rotate-3 ease-in-out duration-[4.5s]' src='/webp/blog-3.webp' alt=''/>
+                                    <img className='cursor-pointer relative w-full hover:scale-105 peer-hover:scale-105 hover:rotate-3 peer-hover:rotate-3 ease duration-[3.5s]' src='/webp/blog-3.webp' alt=''/>
                                 </div>
                                 <div className="flex flex-row items-center w-full mt-3">
                                     <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
@@ -85,43 +84,13 @@ export default function ProjectsPage() {
                                     </p>
                                 </div>
                             </div>
-                        
                             
                             <div className="flex flex-col item-center max-w-[36rem]">
                                 <div className='relative w-full max-w-[36rem] max-h-[43rem] xl:rounded-[2rem] rounded-[1rem] mb-3 overflow-hidden'>
                                     <div className="cursor-pointer peer z-10 absolute top-8 right-8 rounded-full bg-white text-zinc-800 h-24 w-24 p-3 flex justify-center  hover:bg-pink-600 hover:text-zinc-100 ease-in-out duration-200">
                                         <ArrowRightIcon className="font-bold m-auto p-1 h-10 w-auto -rotate-45" />
                                     </div>
-                                    <img className='cursor-pointer relative w-full hover:scale-125 peer-hover:scale-125 hover:rotate-6 peer-hover:rotate-3 ease-in-out duration-[4.5s]' src='/webp/blog-3.webp' alt=''/>
-                                </div>
-                                <div className="flex flex-row items-center w-full mt-3">
-                                    <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
-                                        <p className='w-full m-auto text-base  font-normal'>Technology</p>
-                                    </div>
-                                    <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
-                                        <p className='w-full m-auto text-base  font-normal'>Internship</p>
-                                    </div>
-                                    <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
-                                        <p className='w-full m-auto text-base  font-normal'>Boost</p>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col jw-full">
-                                    <h3 className='text-xl md:text-3xl text-zinc-300 max-w-max my-7'>
-                                        How cold email can scale a business
-                                    </h3>
-                                    <p className="text-sm font-normal text-zinc-400 mb-5">
-                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptatem optio perferendis et consectetur.
-                                    </p>
-                                </div>
-                            </div>
-                        
-                            
-                            <div className="flex flex-col item-center max-w-[36rem]">
-                                <div className='relative w-full max-w-[36rem] max-h-[43rem] xl:rounded-[2rem] rounded-[1rem] mb-3 overflow-hidden'>
-                                    <div className="cursor-pointer peer z-10 absolute top-8 right-8 rounded-full bg-white text-zinc-800 h-24 w-24 p-3 flex justify-center  hover:bg-pink-600 hover:text-zinc-100 ease-in-out duration-200">
-                                        <ArrowRightIcon className="font-bold m-auto p-1 h-10 w-auto -rotate-45" />
-                                    </div>
-                                    <img className='cursor-pointer relative w-full hover:scale-125 peer-hover:scale-125 hover:rotate-6 peer-hover:rotate-3 ease-in-out duration-[4.5s]' src='/webp/blog-3.webp' alt=''/>
+                                    <img className='cursor-pointer relative w-full hover:scale-105 peer-hover:scale-105 hover:rotate-3 peer-hover:rotate-3 ease duration-[3.5s]' src='/webp/blog-3.webp' alt=''/>
                                 </div>
                                 <div className="flex flex-row items-center w-full mt-3">
                                     <div className="flex justify-center mr-5 border-solid border-[1px] border-zinc-300/60 text-zinc-300/80 text-center h-12 px-5 rounded-[10px]">
