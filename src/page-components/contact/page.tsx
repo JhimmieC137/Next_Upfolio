2089d4fb35aa35c3cac15b500f49271b28ce396f
JhimmieC137 <toluwalope.david@gmail.com>
2024-10-17 22:54:17 +0100

    Added community page

--- src/page-components/contact/page.tsx
+++ src/page-components/contact/page.tsx
@@ -1,10 +1,106 @@
+import { CiMail } from "react-icons/ci";
+import { PiPhoneLight } from "react-icons/pi";
+import { RxPerson } from "react-icons/rx";
+
 export default function ContactPage() {
     return (
-        <main className="px-6 py-24 sm:py-32 lg:px-8">
-            <div className="text-center">
-                <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-                    Contact Us
-                </h1>
+        <main className="w-full relative">
+            <div className='max-w-screen m-auto overflow-hidden'>
+                <div className='px-2 lg:px-8 min-h-full'>
+
+                    <section id="hero" className="relative h-[65vh] flex flex-col justify-center items-center mx-auto max-w-2xl md:max-w-3xl lg:max-w-6xl pt-16 pb-20 md:pt-24 md:pb-20 lg:pb-5">
+                        <div className="mb-8 md:mb-2 text-center sm:mb-10 lg:mb-3 flex justify-center">
+                            <p className="text-md  text-zinc-200 font-light">
+                                Home/Contact
+                            </p>
+                        </div>
+                        <div className="text-center">
+                            <h1 className="text-6xl md:text-8xl lg:text-[8rem] font-normal tracking-tight text-yellow-300">
+                                Contact <span className="lg:text-[9rem] text-cyan-600">Us</span>
+                            </h1>
+                        </div>
+                    </section>
+
+
+                    <section className="relative mx-auto max-w-2xl md:max-w-[52rem] mt-5 md:mt-20 xl:mt-0 lg:max-w-[1300px] flex justify-between gap-10">
+                        <div className="w-[83%]">
+                            <img className="w-full rounded-[1rem]" src="/jpg/contact_us.jpg" alt="contact_us"/>
+                        </div>
+                        <form className="w-full px-2" action="">
+                            <div className="w-full flex flex-col justify-between">
+                                <div className="w-full">
+                                    <div className="w-full mb-5 flex flex-col">
+                                        <label className="mb-3 pl-1 text-lg text-zinc-400" htmlFor="name">
+                                            Name
+                                        </label>
+                                        <div className="relative w-full">
+                                            <RxPerson className='absolute text-zinc-400/60 h-10 w-10 top-5 bottom-auto left-4 z-10' />
+                                            <input id="name" type="text" placeholder="John" className="relative placeholder:text-zinc-400/60 text-white/90 w-full pl-[4.2rem] pr-4 text-[1.2rem] border border-zinc-600 rounded-[0.85rem] h-20" required/>
+                                        </div>
+                                    </div>
+                                    <div className="w-full my-5 flex flex-col">
+                                        <label className="mb-3 pl-1 text-lg text-zinc-400" htmlFor="phone">
+                                            Phone
+                                        </label>
+                                        <div className="relative w-full">
+                                            <PiPhoneLight className='absolute text-zinc-400/60 h-10 w-10 top-5 bottom-auto left-4 z-10' />
+                                            <input id="phone" type="text" placeholder="08000000000" className="relative placeholder:text-zinc-400/60 text-white/90 w-full pl-[4.2rem] pr-4 text-[1.2rem] border border-zinc-600 rounded-[0.85rem] h-20" required/>
+                                        </div>
+                                    </div>
+                                    <div className="w-full my-5 flex flex-col">
+                                        <label className="mb-3 pl-1 text-lg text-zinc-400" htmlFor="email">
+                                            Email
+                                        </label>
+                                        <div className="relative w-full">
+                                            <CiMail className='absolute  text-zinc-400/60 h-10 w-10 top-5 bottom-auto left-4 z-10' />
+                                            <input id="email" type="text" placeholder="example@mail.com" className="relative placeholder:text-zinc-400/60 text-white/90 w-full pl-[4.2rem] pr-4 text-[1.2rem] border border-zinc-600 rounded-[0.85rem] h-20" required/>
+                                        </div>
+                                    </div>
+                                    <div className="w-full my-5 flex flex-col">
+                                        <label className="mb-3 pl-1 text-lg text-zinc-400" htmlFor="message">
+                                            Your message
+                                        </label>
+                                        <textarea id='message' rows={3} className="p-4 placeholder:text-zinc-400/60 text-white/90 text-[1.2rem] border border-zinc-600 rounded-[0.85rem]" placeholder="Please write here ... " required/>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <button type="submit" className="w-full h-14 rounded-[0.5rem] bg-pink-700 hover:bg-pink-600 text-center text-[1.2rem] font-normal">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+
+
+                    <section className="relative mx-auto max-w-2xl md:max-w-[52rem] mt-5 md:mt-20 xl:mt-20 lg:max-w-[1300px] flex flex-col justify-between ">
+                        <div className="w-full py-5 flex justify-between gap-10">
+                            <div className="w-2/6 flex justify-center items-center gap-3">
+                                <img src="/png/star.png" className="w-2/6" alt=""/>
+                                <p className="text-sm text-zinc-400">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur voluptatem, mollitia vel aliquam quia distinctio laborum eius, cumque cum.
+                                </p>
+                            </div>
+                            <div className="w-2/6 flex justify-center items-center gap-3">
+                                <img src="/png/star.png" className="w-2/6" alt=""/>
+                                <p className="text-sm text-zinc-400">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Onsequatur voluptatem, mollitia vel aliquam quia distinctio laborum eius, cumque cum.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="w-full rounded-[2rem]">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11855.8621423493!2d3.907607665226474!3d7.418770170261981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039ed5bb43aa999%3A0x77a78d14caa75a20!2sBodija%20Zone%2C%20Ibadan%20200132%2C%20Oyo!5e0!3m2!1sen!2sng!4v1729108743466!5m2!1sen!2sng" 
+                                // width="600" 
+                                // height="450" 
+                                style={{ border: 0, width: '100%', height: '500px', borderRadius: "25px"}} 
+                                allowFullScreen={true} 
+                                loading="lazy" 
+                                referrerPolicy="no-referrer-when-downgrade"
+                            />
+                        </div>
+                    </section>
+
+
+
+                </div>
             </div>
         </main>
     )
--- src/page-components/join-our-community/page.tsx
+++ src/page-components/join-our-community/page.tsx
@@ -1,10 +1,97 @@
 export default function JOCPage() {
     return (
-        <main className="px-6 py-24 sm:py-32 lg:px-8">
-            <div className="text-center">
-                <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-                    Join our community
-                </h1>
+        <main className="w-full relative">
+            <div className='max-w-screen m-auto overflow-hidden'>
+                <div className='px-2 lg:px-8 min-h-full'>
+
+                    <section id="hero" className="relative h-[70vh] flex flex-col justify-center items-center mx-auto max-w-2xl md:max-w-3xl lg:max-w-6xl pt-16 pb-20 md:pt-24 md:pb-20 lg:pb-14">
+                        <div className="mb-8 md:mb-2 text-center sm:mb-10 lg:mb-3 flex justify-center">
+                            <p className="text-md  text-zinc-200 font-light">
+                                Home/Join-Our-Community
+                            </p>
+                        </div>
+                        <div className="text-center">
+                            <h1 className="text-6xl md:text-8xl lg:text-[8rem] font-normal tracking-tight text-yellow-300">
+                                Join Our <br/><span className="lg:text-[9rem] text-cyan-600">Community</span>
+                            </h1>
+                        </div>
+                    </section>
+
+
+                    <section className="relative mx-auto max-w-2xl md:max-w-[52rem] mt-5 md:mt-20 xl:mt-0 lg:max-w-[1300px]">
+                        <div className="w-full -mt-20">
+                            <img className="rounded-[2.5rem]" src="/jpg/hands_together.jpg" alt="team_meeting"/>
+                        </div>
+                    </section>
+
+
+                    <section id="intro" className="relative mx-auto max-w-2xl md:max-w-[52rem] mt-5 md:mt-20 xl:mt-20 lg:max-w-[1300px]  py-0 md:py-20 lg:py-16">
+                        {/* <div className="flex flex-row justify-center md:justify-start ">
+                            <div className="text-left md:max-w-6xl px-5">
+                                <h1 className="text-center md:text-left text-[3rem] md:text-[4.5rem] lg:text-[6rem] xl:text-[6.5rem] 2xl:text-[7.7rem] text-yellow-300">
+                                    Benefits Of
+                                </h1>
+                                <div className="relative w-full flex flex-row justify-start ml-0 md:ml-10 xl:ml-16 -mt-5 md:-mt-7">
+                                    <img className="absolute -left-48 -top-6 md:-top-5 lg:-top-5 w-[29%]" src="/png/meteor.png" alt="icon"/>
+                                    <h2 className="text-center md:text-left text-[3rem] md:text-[4.5rem] lg:text-[6rem] xl:text-[6.5rem] 2xl:text-[7.7rem] text-cyan-600 font-normal">
+                                        Being A Member
+                                    </h2>
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="w-full">
+                            
+                            <div className="flex flex-row justify-between items-center w-full my-10 md:my-24 px-5">
+                                <div className=" px-3 md:px-4 md:p-3 w-[45%]">
+                                    <h2 className="font-light text-zinc-100 text-[3.2rem] leading-tight my-10 pr-3">
+                                        We celebrate birthdays
+                                    </h2>
+                                    <p className="text-lg font-normal text-zinc-400">
+                                        Lorem ipsum dolor itaque autem alias quo natus quasi fugiat eaque ipsa quis voluptate nam numquam, fugit aliquid, ratione cum est? Dolorem consequuntur necessitatibus voluptate, ipsa quis voluptate nam numquam!
+                                    </p>
+                                </div>
+                                <img src="/jpg/birthday.jpg" alt="" className="rounded-2xl w-2/5"/>
+                            </div>
+                            
+                            
+                            <div className="flex flex-row-reverse justify-between items-center w-full md:my-24 px-5">
+                                <div className=" px-3 md:px-4 md:p-3 w-[42%]">
+                                    <h2 className="font-light text-zinc-100 text-[3.2rem] leading-tight my-10 pr-3">
+                                        We celebrate birthdays
+                                    </h2>
+                                    <p className="text-lg font-normal text-zinc-400">
+                                        Lorem ipsum dolor itaque autem alias quo natus quasi fugiat eaque ipsa quis voluptate nam numquam, fugit aliquid, ratione cum est? Dolorem consequuntur necessitatibus voluptate, ipsa quis voluptate nam numquam!
+                                    </p>
+                                </div>
+                                <img src="/jpg/birthday.jpg" alt="" className="rounded-2xl w-2/5"/>
+                            </div>
+                            
+                            
+                            <div className="flex flex-row justify-between items-center w-full md:my-24 px-5">
+                                <div className=" px-3 md:px-4 md:p-3 w-[45%]">
+                                    <h2 className="font-light text-zinc-100 text-[3.2rem] leading-tight my-10 pr-3">
+                                        We celebrate birthdays
+                                    </h2>
+                                    <p className="text-lg font-normal text-zinc-400">
+                                        Lorem ipsum dolor itaque autem alias quo natus quasi fugiat eaque ipsa quis voluptate nam numquam, fugit aliquid, ratione cum est? Dolorem consequuntur necessitatibus voluptate, ipsa quis voluptate nam numquam!
+                                    </p>
+                                </div>
+                                <img src="/jpg/birthday.jpg" alt="" className="rounded-2xl w-2/5"/>
+                            </div>
+
+                        </div>
+                    </section>
+
+
+                    <section id="intro" className="relative mx-auto max-w-2xl md:max-w-[52rem] mt-5 md:mt-20 xl:mt-20 lg:max-w-[1300px]  py-0 md:py-20 lg:py-16">
+                        <div className="w-[95%]">
+
+                        </div>
+                    </section>
+
+
+
+                </div>
             </div>
         </main>
     )
